8583e0c5180f7168d4d475d609cefaee62d6d5e8
Cristian George Rapauzu <cristian.rapauzu@gmail.com>
2018-05-24 01:53:31 -0400

    default singleton scope highlighted,args corrected

--- src/main/java/net/softesco/neonasa/CentralScrutinizer.java
+++ src/main/java/net/softesco/neonasa/CentralScrutinizer.java
@@ -4,22 +4,15 @@ import lombok.Getter;
 import net.softesco.neonasa.dto.NeoSummary;
 import net.softesco.neonasa.strategy.ScrutinizingStrategy;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 /**
  * Scrutinizes the NEO info from REST API responses and
  * accumulates the info (number, greatest, nearest NEO today, plus paging) in NeoSummary.
- * Singleton using Strategy to scrutinize responses and persist NEO summary.
+ * Singleton (default scope of Spring bean) using Strategy to scrutinize responses and persist NEO summary.
  * 
  * @author cristi
  */
 public class CentralScrutinizer {
 
-	private static CentralScrutinizer instance = null;
-	
-	private static Logger logger = LoggerFactory.getLogger(CentralScrutinizer.class);
-	
 	// summary of number, greatest, nearest NEO today, plus paging
 	@Getter
 	private NeoSummary neoSummary;
@@ -28,19 +21,9 @@ public class CentralScrutinizer {
 	@Getter
 	private ScrutinizingStrategy scrutinizingStrategy;
 
-	// Singleton
-	private CentralScrutinizer(ScrutinizingStrategy scrutinizingStrategy, NeoSummary neoSummary) {
+	public CentralScrutinizer(ScrutinizingStrategy scrutinizingStrategy, NeoSummary neoSummary) {
 		this.scrutinizingStrategy = scrutinizingStrategy;
 		this.neoSummary = neoSummary;
 	}
 	
-	// Singleton instance getter
-	public static CentralScrutinizer getInstance(ScrutinizingStrategy scrutinizingStrategy, NeoSummary neoSummary) {
-		if (instance == null) {
-			instance = new CentralScrutinizer(scrutinizingStrategy, neoSummary);
-			logger.info("Strategy: " + scrutinizingStrategy.getDescription());
-		}
-		return instance;
-	}
-
 }
--- src/main/java/net/softesco/neonasa/NeonasaApplication.java
+++ src/main/java/net/softesco/neonasa/NeonasaApplication.java
@@ -4,10 +4,13 @@ import net.softesco.neonasa.dto.NeoSummary;
 import net.softesco.neonasa.strategy.AsynchronousFluxWithBackPressureScrutinizingStrategy;
 import net.softesco.neonasa.strategy.ScrutinizingStrategy;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
 
 /**
  * Use NASA Near Earth Object (NEO) REST API to obtain NEO information like: 
@@ -19,7 +22,9 @@ import org.springframework.context.annotation.Bean;
  */
 @SpringBootApplication
 public class NeonasaApplication {
-		
+	
+	private static Logger logger = LoggerFactory.getLogger(NeonasaApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(NeonasaApplication.class, args);
 	}
@@ -39,17 +44,24 @@ public class NeonasaApplication {
 	}
 
 	@Bean
+	@Scope("singleton") // default scope - just a reminder of the convention-over-configuration advantages
 	public CentralScrutinizer centralScrutinizer(ScrutinizingStrategy scrutinizingStrategy, NeoSummary neoSummary) {
-		return CentralScrutinizer.getInstance(scrutinizingStrategy, neoSummary);
+		return new CentralScrutinizer(scrutinizingStrategy, neoSummary);
 	}
 	
 	@Bean
 	public CommandLineRunner run(CentralScrutinizer centralScrutinizer) throws Exception {
 		return args -> {
-			int firstPageNumber = 910; // must explicitly require all pages by providing parameter "-startpage=1"
-			System.out.println("args: " + ((args.length==0) ? "none (starts scanning from page 910)" : args[0]));
-			centralScrutinizer.getScrutinizingStrategy().obtainNeoCountFromStatistics(centralScrutinizer.getNeoSummary());
-			centralScrutinizer.getScrutinizingStrategy().obtainNeoInfoFromBrowsedPages(centralScrutinizer.getNeoSummary(), firstPageNumber);
+			// must explicitly require all pages by providing parameter "-startpage=1", otherwise start at less-stressing default 910
+			int firstPageNumber = 910;
+			if (args.length > 0) {
+				String[] startpageArg = args[0].split("=");
+				firstPageNumber = Integer.parseInt(startpageArg[1]); 
+			}
+			final ScrutinizingStrategy scrutinizingStrategy = centralScrutinizer.getScrutinizingStrategy();
+			logger.info("Starts scanning from page: " + firstPageNumber + " using strategy: " + scrutinizingStrategy.getDescription());
+			scrutinizingStrategy.obtainNeoCountFromStatistics(centralScrutinizer.getNeoSummary());
+			scrutinizingStrategy.obtainNeoInfoFromBrowsedPages(centralScrutinizer.getNeoSummary(), firstPageNumber);
 		};
 	}
 }
