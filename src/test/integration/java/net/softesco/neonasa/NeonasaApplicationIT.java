1a7db2bd98bf959ff62407fdc239f80efbc87565
Cristian George Rapauzu <cristian.rapauzu@gmail.com>
2018-05-27 00:26:37 -0400

    Revisited Unit/Integration tests separation

---
+++ src/test/integration/java/net/softesco/neonasa/NeonasaApplicationIT.java
@@ -0,0 +1,35 @@
+package net.softesco.neonasa;
+
+import net.softesco.neonasa.dto.NeoSummary;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@TestExecutionListeners(listeners = {DependencyInjectionTestExecutionListener.class})
+@SpringBootTest
+public class NeonasaApplicationIT {
+
+	@Autowired
+	private CentralScrutinizer centralScrutinizer;
+	
+	@Test
+	public void contextLoads() {
+		// Strategy: AsynchronousFluxWithBackPressureScrutinizingStrategy
+		assertThat(centralScrutinizer).isNotNull();
+		assertThat(centralScrutinizer.getScrutinizingStrategy()).isNotNull();
+		assertThat("Asynchronous with Flux and back-pressure".equals(centralScrutinizer.getScrutinizingStrategy().getDescription())).isTrue();
+		final NeoSummary neoSummary = centralScrutinizer.getNeoSummary();
+		assertThat(neoSummary).isNotNull();
+		// persistence in file /tmp/neonasa/neo.summary
+		assertThat(neoSummary.getNeoSummaryChannel()).isNotNull();
+	}
+
+}
